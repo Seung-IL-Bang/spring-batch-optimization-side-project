2d41051df7662bf8634a7fa233b82f7331227206
Seungil <seungilbang@khu.ac.kr>
2025-03-22 16:52:27 +0900

    fix: refactor deliveryCompletedJpaItemReader to use JdbcCursorItemReader and optimize SQL query

--- batch_service/src/main/java/com/project/batch_service/jobs/daily_settle/DailySettlementJobConfig.java
+++ batch_service/src/main/java/com/project/batch_service/jobs/daily_settle/DailySettlementJobConfig.java
@@ -58,7 +58,7 @@ public class DailySettlementJobConfig {
         int CHUNK_SIZE = JobParameterUtils.parseChunkSize(chunkSize);
         return new StepBuilder(PURCHASE_CONFIRMED_STEP, jobRepository)
                 .<OrderProduct, OrderProduct>chunk(CHUNK_SIZE, transactionManager)
-                .reader(purchaseConfirmStepConfig.deliveryCompletedJpaItemReader(null, CHUNK_SIZE))
+                .reader(purchaseConfirmStepConfig.deliveryCompletedJpaItemReader(null))
                 .writer(purchaseConfirmStepConfig.purchaseConfirmedItemWriter())
                 .build();
     }
